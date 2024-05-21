4f677853e904f40b907fc9374a254beb6c0f0f0f
Brandonz07 <brodriguez@sistemasenlinea.com.co>
2024-05-20 21:45:21 -0500

    feat: l4 first exercise added

---
+++ src/br/js/L4-data-structures/The_sum_of_a_range.js
@@ -0,0 +1,25 @@
+function range(start, end, step = 1) {
+  let arr = [];
+  if (step > 0) {
+    for (let i = start; i <= end; i += step) {
+      arr.push(i);
+    }
+  } else {
+    for (let i = start; i >= end; i += step) {
+      arr.push(i);
+    }
+  }
+  return arr;
+}
+
+function sum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+console.log(range(1, 10));
+console.log(range(5, 2, -1));
+console.log(sum(range(1, 10)));
