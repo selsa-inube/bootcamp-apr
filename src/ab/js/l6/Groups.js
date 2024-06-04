9d1761100578f9994f06e4eb48bc0a24424df5fa
Angelbt12 <angeldavidbaracaldo056@gmail.com>
2024-06-04 17:14:37 -0500

    feat: refactoring done

--- src/ab/js/l6/Groups.js
+++ src/ab/js/l6/Groups.js
@@ -1,39 +1,37 @@
 class Group {
-    constructor() {
-      this.members = [];
-    }
-  
-    add(value) {
-      if (!this.has(value)) {
-        this.members.push(value);
-      }
-    }
-  
-    delete(value) {
-      const index = this.members.indexOf(value);
-      if (index !== -1) {
-        this.members.splice(index, 1);
-      }
-    }
-  
-    has(value) {
-      return this.members.includes(value);
-    }
-  
-    static from(iterable) {
-      const group = new Group();
-      for (let value of iterable) {
-        group.add(value);
-      }
-      return group;
+  constructor() {
+    this.members = [];
+  }
+
+  add(value) {
+    if (!this.has(value)) {
+      this.members.push(value);
     }
   }
-  
 
-  let group = Group.from([10, 20 ]);
-  console.log(group.has(10)); 
-  console.log(group.has(30)); 
-  group.add(10);
-  group.delete(10);
-  console.log(group.has(10)); 
-  
+  delete(value) {
+    if (this.has(value)) {
+      const index = this.members.indexOf(value);
+      this.members.splice(index, 1);
+    }
+  }
+
+  has(value) {
+    return this.members.includes(value);
+  }
+
+  static from(iterable) {
+    const group = new Group();
+    for (let value of iterable) {
+      group.add(value);
+    }
+    return group;
+  }
+}
+
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+console.log(group.has(30));
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
