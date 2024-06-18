ad501e95861464389d67f3b03e7f2646c139a88c
Brandonz07 <brodriguez@sistemasenlinea.com.co>
2024-06-18 10:56:37 -0500

    feat: router added

--- src/br/React/router/src/main.jsx
+++ src/br/React/router/src/main.jsx
@@ -7,13 +7,18 @@ import {
 import "./index.css";
 import ErrorPage from "./error-page";
 import Contact,{
-  loader as contactLoader
+  loader as contactLoader,
+  action as contactAction,
 } from "./routes/contact";
 import Root, {
   action as rootAction,
   loader as rootLoader,
 } from "./routes/root";
-import EditContact from "./routes/edit";
+import EditContact,{
+  action as editAction,
+} from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes";
 
 const router = createBrowserRouter([
   {
@@ -24,18 +29,31 @@ const router = createBrowserRouter([
     action: rootAction,
     children: [
       {
-        path: "contacts/:contactId",
-        element: <Contact />,
-        loader: contactLoader,
-      },
+      errorElement: <ErrorPage />,
+      children: [
+        { index: true, element: <Index /> },
+        {
+          path: "contacts/:contactId",
+          element: <Contact />,
+          loader: contactLoader,
+          action: contactAction,
+        },
       {
         path: "contacts/:contactId/edit",
         element: <EditContact />,
         loader: contactLoader,
-      }
-    ]
+        action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
+      
+      },
+    ],
   },
-  
+],
+},
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
